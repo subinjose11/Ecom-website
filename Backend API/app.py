9d9d585f513f403d4c544bfa7b414bcc5464f8d6
subinjose11 <subinjose911@gmail.com>
2024-02-29 11:17:40 +0530

    file updated with customer details post method

--- Backend API/app.py
+++ Backend API/app.py
@@ -19,7 +19,28 @@ def db_connection():
     except pymysql.Error as e:
         print(e)
     return conn
-
+@app.route('/customer_details', methods=['POST',])
+def customer_details():
+    conn = db_connection()
+    cursor = conn.cursor()
+    
+    if request.method == 'POST':
+        try:
+            new_customer_name = request.form['customer_name']
+            new_mobile_number = request.form['mobile_number']
+            new_dateofbirth = request.form['date_of_birth']
+            new_email = request.form['email']
+            new_address = request.form['address']
+            new_pincode = request.form['pin_code']
+            
+        except KeyError as e:
+            return jsonify({'error': f'KeyError: {e}'}), 400 
+                
+        query="insert into customer_details (customer_name,mob_number,date_of_birth,email,address,pin_code) values ('{}','{}','{}','{}','{}','{}');".format(new_customer_name,new_mobile_number,new_dateofbirth,new_email,new_address,new_pincode)
+        
+        cursor.execute(query)
+        conn.commit()
+        return jsonify({'message': 'Customer details registered successfully'}), 200
 
 if __name__=='__main__':
     app.run(debug=True)
