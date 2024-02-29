abcedbf72a1c65c44e3599f4fc1cd08ca4251473
subinjose11 <subinjose911@gmail.com>
2024-02-29 15:21:34 +0530

    file updated with customer details get  method

--- Backend API/app.py
+++ Backend API/app.py
@@ -19,7 +19,7 @@ def db_connection():
     except pymysql.Error as e:
         print(e)
     return conn
-@app.route('/customer_details', methods=['POST',])
+@app.route('/customer_details', methods=['POST','GET'])
 def customer_details():
     conn = db_connection()
     cursor = conn.cursor()
@@ -37,10 +37,20 @@ def customer_details():
             return jsonify({'error': f'KeyError: {e}'}), 400 
                 
         query="insert into customer_details (customer_name,mob_number,date_of_birth,email,address,pin_code) values ('{}','{}','{}','{}','{}','{}');".format(new_customer_name,new_mobile_number,new_dateofbirth,new_email,new_address,new_pincode)
-        
+
         cursor.execute(query)
         conn.commit()
+        conn.close()
         return jsonify({'message': 'Customer details registered successfully'}), 200
+    
+    elif request.method=='GET':
+        customers=[]
+        cursor.execute("SELECT * FROM customer_details") 
+        for row in cursor.fetchall():
+            customers.append(row)
+        conn.close()
+        return jsonify(customers)           
+
 
 if __name__=='__main__':
     app.run(debug=True)
